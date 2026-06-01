4fe93a52fad01ca2eeb9efb522ae87c830dd3b05
Cherepok Timophey <RjTima@gmail.com>
2026-06-01 16:41:45 +0300

    Add GAS character base

---
+++ Source/GASEnemyCombat/Characters/GASCharacterBase.cpp
@@ -0,0 +1,116 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Characters/GASCharacterBase.h"
+
+#include "AbilitySystem/CombatAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameplayAbilitySpec.h"
+#include "GameplayEffect.h"
+
+AGASCharacterBase::AGASCharacterBase()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AttributeSet = CreateDefaultSubobject<UCombatAttributeSet>(TEXT("AttributeSet"));
+
+	bIsDead = false;
+}
+
+UAbilitySystemComponent* AGASCharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGASCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+		if (HasAuthority())
+		{
+			GiveDefaultAbilities();
+			ApplyDefaultEffects();
+		}
+	}
+}
+
+void AGASCharacterBase::GiveDefaultAbilities()
+{
+	if (!AbilitySystemComponent || !HasAuthority())
+	{
+		return;
+	}
+
+	for (const TSubclassOf<UGameplayAbility>& AbilityClass : DefaultAbilities)
+	{
+		if (!AbilityClass)
+		{
+			continue;
+		}
+
+		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(AbilityClass, 1, INDEX_NONE, this));
+	}
+}
+
+void AGASCharacterBase::ApplyDefaultEffects()
+{
+	if (!AbilitySystemComponent || !HasAuthority())
+	{
+		return;
+	}
+
+	FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	for (const TSubclassOf<UGameplayEffect>& EffectClass : DefaultEffects)
+	{
+		if (!EffectClass)
+		{
+			continue;
+		}
+
+		FGameplayEffectSpecHandle EffectSpecHandle = AbilitySystemComponent->MakeOutgoingSpec(EffectClass, 1.0f, EffectContext);
+		if (!EffectSpecHandle.IsValid())
+		{
+			continue;
+		}
+
+		AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	}
+}
+
+void AGASCharacterBase::Die()
+{
+	if (bIsDead)
+	{
+		return;
+	}
+
+	bIsDead = true;
+
+	if (UCharacterMovementComponent* MovementComponent = GetCharacterMovement())
+	{
+		MovementComponent->StopMovementImmediately();
+		MovementComponent->DisableMovement();
+	}
+
+	if (UCapsuleComponent* CharacterCapsule = GetCapsuleComponent())
+	{
+		CharacterCapsule->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
+	OnDeath();
+}
+
+void AGASCharacterBase::OnDeath_Implementation()
+{
+}
+
+bool AGASCharacterBase::IsDead() const
+{
+	return bIsDead;
+}
---
+++ Source/GASEnemyCombat/Characters/GASCharacterBase.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "GASCharacterBase.generated.h"
+
+class UAbilitySystemComponent;
+class UCombatAttributeSet;
+class UGameplayAbility;
+class UGameplayEffect;
+
+/**
+ * Base character with Gameplay Ability System support.
+ */
+UCLASS()
+class GASENEMYCOMBAT_API AGASCharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AGASCharacterBase();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	UFUNCTION(BlueprintCallable, Category = "GAS")
+	virtual void GiveDefaultAbilities();
+
+	UFUNCTION(BlueprintCallable, Category = "GAS")
+	virtual void ApplyDefaultEffects();
+
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	virtual void Die();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Combat")
+	void OnDeath();
+
+	UFUNCTION(BlueprintPure, Category = "Combat")
+	bool IsDead() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	UCombatAttributeSet* AttributeSet;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS")
+	TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS")
+	TArray<TSubclassOf<UGameplayEffect>> DefaultEffects;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+	bool bIsDead;
+};
